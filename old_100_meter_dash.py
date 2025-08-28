f17e1e136a38eea0dea6a748e8385dc478800088
CalvinGro <153310569+CalvinGro@users.noreply.github.com>
2025-08-28 17:00:40 -0500

    100_meter_dash: remove unneeded functions

--- old_100_meter_dash.py
+++ old_100_meter_dash.py
@@ -2,24 +2,7 @@
 import math
 # getcontext().prec = 40
 
-def check_back(ref_points:list[tuple[float,float]], current:int) -> bool:
-    meters = 0
-    for i, point in enumerate(ref_points):
-        if i > current:
-            break
-        meters += point[0]
-        if meters >= 100:
-            return True
-    return False
-    
-def check_forward(ref_points:list[tuple[float,float]], current:int) -> bool:
-    meters = 0
-    for point in ref_points[current:]:
-        meters += point[0]
-    if meters >= 100:
-        return True
-    else:
-        return False
+
         
 
 def find_distance(x:float, y:float, prev_dist:tuple[float,float]) -> float:
@@ -52,44 +35,40 @@ def find_fastest_100(ref_points:tuple[float,float]) -> float:
 
     for i,new_p in enumerate(ref_points):
 
+        time = 0
+        dist = 0
+        for new_p in ref_points[i:]:
+            if dist >= 100:
+                break
+            elif (dist + new_p[0]) < 100:
+                dist += new_p[0]
+                time += new_p[1]
+            else:
+                ad_dist = 100 - dist
+                time += new_p[1]*(ad_dist/new_p[0])
+                dist += ad_dist
+        if time < fastest and dist == 100:
+            fastest = time
+    
+        time = 0
+        dist = 0
+        for prev_p in ref_points[i::-1]:
+            if dist >= 100:
+                break
 
-        if check_forward(ref_points, i):
-            time = 0
-            dist = 0
-            for new_p in ref_points[i:]:
-                if dist >= 100:
-                    break
-                elif (dist + new_p[0]) < 100:
-                    dist += new_p[0]
-                    time += new_p[1]
-                else:
-                    ad_dist = 100 - dist
-                    time += new_p[1]*(ad_dist/new_p[0])
-                    dist += ad_dist
-            if time < fastest:
-                fastest = time
-        
+            # if it can include the full segment
+            elif (dist + prev_p[0]) < 100:
+                dist += prev_p[0]
+                time += prev_p[1]
 
-        if check_back(ref_points, i):
-            time = 0
-            dist = 0
-            for prev_p in ref_points[i::-1]:
-                if dist >= 100:
-                    break
-
-                # if it can include the full segment
-                elif (dist + prev_p[0]) < 100:
-                    dist += prev_p[0]
-                    time += prev_p[1]
-
-                # adding part of a segment
-                else:
-                    ad_dist = 100 - dist
-                    time += prev_p[1]*(ad_dist/prev_p[0])
-                    dist += ad_dist
-                    
-            if time < fastest:
-                fastest = time
+            # adding part of a segment
+            else:
+                ad_dist = 100 - dist
+                time += prev_p[1]*(ad_dist/prev_p[0])
+                dist += ad_dist
+                
+        if time < fastest and dist == 100:
+            fastest = time
 
 
     return fastest
